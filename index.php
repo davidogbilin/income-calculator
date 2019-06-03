6252182df594a4abfb56924d069900eda379b445
David <davidgbilin@gmail.com>
2019-06-03 21:29:12 +0100

    changes committed successfully

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
+<html lang="us">
+	<head>
+	<title>INCOME CALCULATOR</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/all.css">
+	<link rel="stylesheet" href="css/all.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	</head>
+
 <body>
+		<div class="container">
+			<h2 class="text-center">Income Calculator</h2>
+			<hr><br>
+			
+			<form action="" method="post">
+				<div class="form-group">
+					<h4 class="text-center label_font_size">Name</h4>
+					<input type="text" name="income" class="form-control input_size">
+					<br>
+					<h4 class="text-center label_font_size">Enter Income below</h4>
+					<input type="text" name="income" class="form-control input_size">
+					<br>
+					<h4 class="text-center label_font_size">Select month</h4>
+					<select class="custom-select input_size">
+						<option selected>Select month</option>
+						<option>January</option>
+						<option>February</option>
+						<option>March</option>
+						<option>April</option>
+						<option>May</option>
+						<option>June</option>
+						<option>July</option>
+						<option>August</option>
+						<option>September</option>
+						<option>October</option>
+						<option>November</option>
+						<option>December</option>
+					</select>
+				</div>
+			</form>
+
+		</div>
+
+
 
 </body>
 </html>
